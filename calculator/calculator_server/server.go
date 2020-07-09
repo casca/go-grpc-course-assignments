b8ae2a22ae1aa0f8fb3bfacf2e4ac2f8acbadda1
casca <8927157+casca@users.noreply.github.com>
2020-07-09 08:36:03 +0200

    enable reflection on server

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -12,6 +12,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -113,6 +114,8 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
