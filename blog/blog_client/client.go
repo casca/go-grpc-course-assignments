16777b625b059a30da07027ff4dc46c8a113a82b
casca <8927157+casca@users.noreply.github.com>
2020-07-10 08:37:19 +0200

    implement CreateBlog service

---
+++ blog/blog_client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-grpc-course/blog/blogpb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Blog Client Started")
+
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Milo",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v\n", err)
+	}
+	fmt.Printf("Blog has been created: %v\n", createBlogRes)
+}
